b04e5d52e834adcb31751b09374f93506c543999
Akirachix <alacritynjeri@gmail.com>
2015-06-18 12:43:42 +0300

    file

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+  <html lang="en">
+       <head>
+             <title>Form</title>
+       </head>
+        <body>
+    
+               <form action="register.php" method="post">
+                 username: <input type="text" name="username" value="" /><br />
+                 password: <input type="password" name="password" value="" /><br />
+                    <br />
+                  <input type="submit" name="submit" value="submit" />
+                 </form>
+       </body>
+     </html>
